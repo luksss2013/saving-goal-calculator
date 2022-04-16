e48b52554bd0e85d61ffca7bab60f75314e94977
Lucas Britto da Silva <lucasbritto123@hotmail.com>
2022-04-16 00:46:08 -0300

    Layout component implementation

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,12 @@
+import styles from './Layout.module.scss';
+
+export default function Layout() {
+    
+    return (
+        <div className={styles.container}>
+            <div>teste 1</div>
+
+            <div>teste 2</div>
+        </div>
+    );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Layout from './components/layout';
 import './index.css';
 
 const root = ReactDOM.createRoot(
@@ -8,6 +9,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    Hello world !
+    <Layout />
   </React.StrictMode>
 );
